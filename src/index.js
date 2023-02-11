88409bfdc1e5fc37d25a74fd13dbca9465dbaea9
tanzilalgummilang <tanzilalgummilang@gmail.com>
2023-02-11 23:31:15 +0700

    bikin modular

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { run } from "./app/app.js";
+import { AlertService } from "./app/alert.service.js";
+import { CalculatorService } from "./app/calculator.service.js";
+import { JokesService } from "./app/jokes.service.js";
+// import { AlertService } from "./alert.service";
+// import { CalculatorService } from "./calculator.service"; 
+// import { JokesService } from "./jokes.service";
+
+const alertService = new AlertService()
+const calculatorService = new CalculatorService()
+const jokesService = new JokesService()
+run(alertService, calculatorService, jokesService);
